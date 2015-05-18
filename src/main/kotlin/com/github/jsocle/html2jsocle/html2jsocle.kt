c32db5c5442defcadd8e30ff901993ec8a05bf20
vamf12 <vamf12@gmail.com>
2015-05-18 13:25:26 +0900

    add full html test

--- src/main/kotlin/com/github/jsocle/html2jsocle/html2jsocle.kt
+++ src/main/kotlin/com/github/jsocle/html2jsocle/html2jsocle.kt
@@ -5,8 +5,11 @@ import org.w3c.dom.Node
 import java.io.ByteArrayInputStream
 import kotlin.dom.*
 
-class JScoleHtmlElement(private val element: Element) {
-    private val tagName: String = element.getTagName().replaceAll("^[a-z]") { it.group().toUpperCase() }
+class JScoleHtmlElement(private val element: Element, private val depth: Int = 0) {
+    private val indent = (1..depth).map { "    " } .join(separator = "")
+    private val tagName: String =
+            if (depth == 0) element.getTagName().replaceAll("^[a-z]") { it.group().toUpperCase() }
+            else element.getTagName()
     private val attrs: Map<String, String>
     private val children: List<Node>
 
@@ -21,25 +24,57 @@ class JScoleHtmlElement(private val element: Element) {
         if (singleTextNode) {
             children = listOf()
         } else {
-            children = element.children()
+            children = element.children().filter { !it.isText() || it.text.trim().isNotEmpty() }
         }
 
-        val attrs = hashMapOf<String, String>()
+        val attrs = linkedMapOf<String, String>()
+        val namedNodeMap = element.getAttributes()
+        for (i in (namedNodeMap.length - 1) downTo 0) {
+            val node = namedNodeMap.item(i)
+            attrs[node.nodeName] = node.text
+        }
         if (singleTextNode) {
             attrs["text_"] = element.children()[0].text
         }
         this.attrs = attrs
     }
 
+
     fun render(): String {
-        var response = ""
-        response += tagName
-        response += attrs.toList().map { "${it.first}=\"${it.second}\"" }.join(separator = " ", prefix = "(", postfix = ")")
-        return response
+        var response = StringBuilder()
+        response.append("$indent$tagName")
+
+        if (attrs.size() > 0) {
+            response.append(
+                    attrs.toList()
+                            .map { "${it.first}=\"${it.second}\"" }
+                            .join(separator = " ", prefix = "(", postfix = ")")
+            )
+        }
+
+        if (children.size() > 0) {
+            response.append(" {\n")
+            val childrenBody = children
+                    .map {
+                        if (it.isText()) {
+                            return@map it.text
+                        }
+                        when (it) {
+                            is Element -> JScoleHtmlElement(it, depth + 1).render()
+                            else -> throw IllegalArgumentException("" + it)
+                        }
+                    }
+                    .join("\n")
+            response.append(childrenBody)
+            response.append("\n")
+            response.append("$indent}")
+        }
+
+        return response.toString()
     }
 }
 
 fun convert(source: String): String {
     val document = parseXml(ByteArrayInputStream(source.toByteArray()))
-    return document.elements.map { JScoleHtmlElement(it).render() }.join("\n")
+    return JScoleHtmlElement(document.documentElement!!).render()
 }
--- src/test/kotlin/com/github/jsocle/html2jsocle/Html2JSocleTest.kt
+++ src/test/kotlin/com/github/jsocle/html2jsocle/Html2JSocleTest.kt
@@ -8,5 +8,61 @@ class Html2JSocleTest {
     fun testElement() {
         Assert.assertEquals("H1(text_=\"Hello world!\")", convert("<h1>Hello world!</h1>"))
     }
+
+    Test
+    fun testNested() {
+        val html = """
+<body>
+    <h1>Hello world!</h1>
+</body>
+"""
+        val kt = """Body {
+    h1(text_="Hello world!")
+}"""
+        Assert.assertEquals(kt, convert(html))
+    }
+
+    Test
+    fun testNestedMultiples() {
+        val html = """
+<head>
+    <link type="text/css" rel="stylesheet" href="/site.css"/>
+    <title>Hello</title>
+</head>
+"""
+        val kt = """Head {
+    link(type="text/css" rel="stylesheet" href="/site.css")
+    title(text_="Hello")
+}"""
+        Assert.assertEquals(kt, convert(html))
+    }
+
+    Test
+    fun testHtml() {
+        val html = """
+<html>
+
+<head>
+    <link type="text/css" rel="stylesheet" href="/site.css"/>
+    <title>Hello</title>
+</head>
+
+<body>
+<h1>Hello world!</h1>
+</body>
+
+</html>
+"""
+        val kt = """Html {
+    head {
+        link(type="text/css" rel="stylesheet" href="/site.css")
+        title(text_="Hello")
+    }
+    body {
+        h1(text_="Hello world!")
+    }
+}"""
+        Assert.assertEquals(kt, convert(html))
+    }
 }
 
