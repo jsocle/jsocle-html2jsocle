a17fdff71ecc0e4e0daa9606839a22ee197f19db
vamf12 <vamf12@gmail.com>
2015-05-26 22:44:45 +0900

    add correct url for form

--- src/main/kotlin/com/github/jsocle/html2jsocle/App.kt
+++ src/main/kotlin/com/github/jsocle/html2jsocle/App.kt
@@ -3,23 +3,20 @@ package com.github.jsocle.html2jsocle
 import com.github.jsocle.JSocle
 import com.github.jsocle.html.elements.Html
 import com.github.jsocle.request
+import com.github.jsocle.requests.handlers.RequestHandler0
 
 public class App : JSocle() {
-    init {
-        route("/") { ->
-            val html = request.parameter("html")?.trim() ?: ""
-            val includeBody = request.parameter("includeBody") != null
-            val kt = if (html.length() == 0) "" else {
-                convert(html, includeBody = includeBody)
-            }
-            return@route Html {
-                body {
-                    form(method = "post") {
-                        textarea(name = "html", text_ = html)
-                        input(type = "checkbox", name = "includeBody", value = "includeBody", checked = if (includeBody) "true" else null)
-                        button(text_ = "convert")
-                        pre(text_ = kt)
-                    }
+    val index: RequestHandler0<Html> = route("/") { ->
+        val html = request.parameter("html")?.trim() ?: ""
+        val includeBody = request.parameter("includeBody") != null
+        val kt = if (html.length() == 0) "" else convert(html, includeBody = includeBody)
+        return@route Html {
+            body {
+                form(method = "post", action = index.url()) {
+                    textarea(name = "html", text_ = html)
+                    input(type = "checkbox", name = "includeBody", value = "includeBody", checked = if (includeBody) "true" else null)
+                    button(text_ = "convert")
+                    pre(text_ = kt)
                 }
             }
         }
