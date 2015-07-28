afeb79935945e5cf251132dc971f441c59b95e28
vamf12 <vamf12@gmail.com>
2015-07-29 05:58:56 +0900

    apply form

--- src/main/kotlin/com/github/jsocle/html2jsocle/App.kt
+++ src/main/kotlin/com/github/jsocle/html2jsocle/App.kt
@@ -1,28 +1,35 @@
 package com.github.jsocle.html2jsocle
 
 import com.github.jsocle.JSocle
+import com.github.jsocle.form.BooleanField
+import com.github.jsocle.form.Form
+import com.github.jsocle.form.TextareaField
 import com.github.jsocle.html.elements.Html
-import com.github.jsocle.request
 import com.github.jsocle.requests.handlers.RequestHandler0
 
-public class App : JSocle() {
-    val index: RequestHandler0<Html> = route("/") { ->
-        val html = request.parameter("html")?.trim() ?: ""
-        val includeBody = request.parameter("includeBody") != null
-        val kt = if (html.length() == 0) "" else convert(html, includeBody = includeBody)
-        return@route Html {
+public object app : JSocle() {
+    private val index: RequestHandler0<Html> = route("/") { ->
+        val form = object : Form() {
+            val html by TextareaField()
+            val includeBody by BooleanField()
+        }
+        val kt =
+                if (form.html.value?.length() ?: 0 > 0) convert(form.html.value!!, form.includeBody.value ?: false)
+                else ""
+        Html {
             body {
                 form(method = "post", action = index.url()) {
-                    textarea(name = "html", text_ = html)
-                    input(type = "checkbox", name = "includeBody", value = "includeBody", checked = if (includeBody) "true" else null)
+                    +form.html.render()
+                    +form.includeBody.render()
                     button(text_ = "convert")
                     pre(text_ = kt)
                 }
             }
         }
     }
+
 }
 
 fun main(args: Array<String>) {
-    App().run(config.port)
+    app.run(config.port)
 }
